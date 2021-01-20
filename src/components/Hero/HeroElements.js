dac4c6b9720b4cb14f2726c7cc13b9b5e7df6b6a
Nurmuhammad Ergashev <ergashev.nurik01@mail.ru>
2021-01-20 22:18:59 +0500

    Css BUgs

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -12,6 +12,7 @@ export const HeroSection = styled.section `
 export const HeroImage = styled.img `
     width: 100%;
     height: 100%;
+    object-fit: cover;
 
 `
 export const HeroH1 = styled.h1 `
